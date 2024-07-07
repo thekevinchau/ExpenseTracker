d8812167581f29031eb74800e6d79eb8984daa02
KEVIN CHAU <kevinch@udel.edu>
2024-07-07 17:12:38 -0400

    Hashed expenses into their categories so that they may be reduced

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -13,10 +13,33 @@ interface BarChartProps {
   expenses: Expense[];
 }
 
+interface ExpensesByCategory {
+  [category: string]: number;
+}
+
 export const BarComponent = ({ expenses }: BarChartProps) => {
+  //General Idea: hash all expense objects into new objects with the keys being the categories and the value being the sum of expense cost
+  const convertHashToArray = () => {
+    const ExpensesByCategory = expenses.reduce((acc: ExpensesByCategory, expense: Expense) => {
+      if(!acc[expense.category]){
+        acc[expense.category] = 0;
+      }
+      acc[expense.category] += expense.cost
+      return acc;
+    }, {})
+    const ExpensesByCategoryArray = Object.keys(ExpensesByCategory).map((category: string) => ({
+      category,
+      cost: ExpensesByCategory[category]
+    }))
+    return ExpensesByCategoryArray
+  }
+
+  const expensesReducer = convertHashToArray();
+  console.log(expensesReducer)
+
   return (
     <ResponsiveContainer width="100%" height="100%" className="mt-3">
-      <BarChart width={200} height={200} data={expenses} margin={{ right: 60 }}>
+      <BarChart width={200} height={200} data={expensesReducer} margin={{ right: 60 }}>
         <CartesianGrid strokeDasharray={"3 3"} />
         <XAxis dataKey={"category"}></XAxis>
         <YAxis />
