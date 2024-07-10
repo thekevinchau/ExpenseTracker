2dcf8b592663f1b718c6dc5d71b60904c1236a18
Kevin Chau <Kevin Chau@DESKTOP-TA19N7F>
2024-07-10 05:02:35 -0400

    Added function to remove/add an expense

--- src/components/(primary_display)/Display.tsx
+++ src/components/(primary_display)/Display.tsx
@@ -33,7 +33,7 @@ export default function Display({toggleSettings,categories,}: DisplayProps): JSX
   const [ExpenseObj, setExpenseObj] = useState<Expense>(emptyExpense);
   const [totalBalance, setTotalBalance] = useState<number>(0.0);
   const [incomingBalance, setIncomingBalance] = useState<number>(0);
-  const [outgoingBalance, setOutGoingBalance] = useState<number>(0);
+  const [outgoingBalance, setOutGoingBalance] = useState<number>(450.00);
   const [expenseArray, setExpenseArray] = useState<Expense[]>([]);
   const [pageState, setPageState] = useState<string>("listView");
   const isExpenseArrayPopulated = pageState === "listView" && expenseArray.length > 0;
@@ -50,6 +50,7 @@ export default function Display({toggleSettings,categories,}: DisplayProps): JSX
       alert("You must fill out all fields of this expense!");
     }
   };
+  
 
   const calculateBalance = (transactionType: string) => {
     if (transactionType === "incoming") {
@@ -83,7 +84,7 @@ export default function Display({toggleSettings,categories,}: DisplayProps): JSX
       {pageState === "stats" && (
         <ChartDisplay expenses={expenseArray} ExpenseTypeObject={{incoming: incomingBalance, outgoing: outgoingBalance}}></ChartDisplay>
       )}
-      {isExpenseArrayPopulated && pageState === "listView" && (<ExpenseMapping expenses={expenseArray} setExpenseArray={setExpenseArray}></ExpenseMapping>
+      {isExpenseArrayPopulated && pageState === "listView" && (<ExpenseMapping expenses={expenseArray} setExpenseArray={setExpenseArray} setIncomingBalance={setIncomingBalance} setOutGoingBalance={setOutGoingBalance} balances={{incoming: incomingBalance, total: totalBalance, outgoing: outgoingBalance}}></ExpenseMapping>
       )}
       {isExpenseArrayPopulated === false && pageState === "listView" && <EmptyTransaction></EmptyTransaction>}
     </div>
--- src/components/(primary_display)/ExpenseMapping.tsx
+++ src/components/(primary_display)/ExpenseMapping.tsx
@@ -4,14 +4,34 @@ import { Expense } from "./Display";
 interface ExpenseMappingProps {
   expenses: Expense[];
   setExpenseArray: (expenses: Expense[]) => void;
+  setIncomingBalance: (num: number) => void;
+  setOutGoingBalance: (num: number) => void;
+  balances: {
+    incoming: number,
+    outgoing: number,
+    total: number
+  }
 }
 
-export const ExpenseMapping = ({ expenses, setExpenseArray }: ExpenseMappingProps) => {
+export const ExpenseMapping = ({ expenses, setExpenseArray, setIncomingBalance, setOutGoingBalance, balances}: ExpenseMappingProps) => {
   const expenseArrayWithIDs = expenses.map((expense: Expense, id: number) => expense.id = id+1)
 
   function removeExpense(idToBeDeleted: number){
-    const filteredExpenses = expenses.filter((expense: Expense) => expense.id !== idToBeDeleted)
+    //found the removed expense, now we need to extract its properties and change values accordingly
+    const findRemovedExpense = expenses.find((expense: Expense) => expense.id === idToBeDeleted)
+    //if the type is incoming, we should remove it from "incoming" and add back the amount to the total balance.
+    if (findRemovedExpense?.type === "outgoing"){
+      const difference = balances.outgoing - findRemovedExpense.cost;
+      setOutGoingBalance(difference);
+    }
+    else if (findRemovedExpense?.type === "incoming"){
+      const difference = balances.incoming - findRemovedExpense?.cost;
+      setIncomingBalance(difference);
+    }
+
+    const filteredExpenses = expenses.filter((expense: Expense) => expense !== findRemovedExpense)
     setExpenseArray(filteredExpenses);
+
   }
 
   return (
